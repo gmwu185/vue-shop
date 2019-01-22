8f84bab5b67fe3e5445b4f9b95eda8a7706fa3a7
JimmyWu <gmwu185@gmail.com>
2019-01-23 00:41:54 +0800

    完成：90. Vue 中運用 Bootstrap 及 jQuery

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 套件內容
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import 'bootstrap';
+
+// 自定義內容
 import App from './App';
 import router from './router';
 
